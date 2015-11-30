026b8c5aedfc4f65ba102631cadac654399326df
mmfe8 <mmfe8@lppcwks02.physics.harvard.edu>
2015-11-30 18:16:14 -0500

    I am ready to begin

---
+++ slim_down.sh
@@ -0,0 +1,27 @@
+#
+# slim_down.sh : shell script for taking AZ FW to the barebones necessities
+#
+# source ~/Downloads/MMFE8/slim_down.sh
+#
+
+topdir="/home/mmfe8/Downloads/X-3-slimmingdown"
+
+echo rm -f ${topdir}/*.jou
+echo rm -f ${topdir}/*.log
+echo rm -f ${topdir}/*.xlsx
+echo rm -f ${topdir}/*.zip
+
+echo rm -f ${topdir}/*.pyc
+echo rm -f ${topdir}/*/*.pyc
+echo rm -f ${topdir}/*/*/*.pyc
+echo rm -f ${topdir}/*/*/*/*.pyc
+echo rm -f ${topdir}/*/*/*/*/*.pyc
+
+echo rm -f ${topdir}/*~
+echo rm -f ${topdir}/*/*~
+echo rm -f ${topdir}/*/*/*~
+echo rm -f ${topdir}/*/*/*/*~
+echo rm -f ${topdir}/*/*/*/*/*~
+
+echo rm -rf ${topdir}/X-3.srcs/sources_1/ip/*
+
