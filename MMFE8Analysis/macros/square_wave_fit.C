c74a0edac5241255629431abe28933a196efd07f
Ben Garber, LPPC <ben.garber@cern.ch>
2016-05-23 17:46:46 -0700

    Small changes to CKTP fit

--- MMFE8Analysis/macros/square_wave_fit.C
+++ MMFE8Analysis/macros/square_wave_fit.C
@@ -54,20 +54,21 @@ void square_wave_fit(){
 
     if ((vmm == base->VMM) && (pdac == base->PDAC) && (base->CKTPrunning)) {
       hist->Fill(float(base->XADC) / 4096.0);
+      // printf("%f",float(base->XADC / 4096.0));
     }
   }
 
-  // TH1D* low_hist = hist->Clone("low peak");
-  // TH1D* high_hist = low_hist->Clone("high peak");
+  TH1D* low_hist = hist->Clone("low peak");
+  TH1D* high_hist = low_hist->Clone("high peak");
   double hist_mean = hist->GetMean();
-  // low_hist->GetXaxis()->SetRange(min_V, hist_mean);
-  // high_hist->GetXaxis()->SetRange(hist_mean, max_V);
+  low_hist->GetXaxis()->SetRange(min_V, hist_mean);
+  high_hist->GetXaxis()->SetRange(hist_mean, max_V);
 
   if Gaussian_fit {
     TF1* low_fit = new TF1("low fit", "gaus", min_V, hist_mean);
     TF1* high_fit = new TF1("high fit", "gaus", hist_mean, max_V);
-    hist->Fit("low fit", "R");
-    hist->Fit("high fit", "R");
+    low_hist->Fit("low fit", "R");
+    low_hist->Fit("high fit", "R");
     // TF1* low_fit = low_hist->GetFunction("gaus");
     // TF1* high_fit = high_hist->GetFunction("gaus");
     // low_fit->SetLineStyle(5);
@@ -99,10 +100,10 @@ void square_wave_fit(){
   hist->GetXaxis()->SetRangeUser(0.0, //mean[1]+(6*stdev[1]));
                                 0.5);
 
-  // if Gaussian_fit {
-  //   low_fit->Draw("same");
-  //   high_fit->Draw("same");
-  // }
+  if Gaussian_fit {
+    low_fit->Draw("same");
+    high_fit->Draw("same");
+  }
   // for (int i = 0; i < num_pdacs; i++){
   //   hists[i]->SetLineColorAlpha(colors[i], 0.8);
   //   hists[i]->SetLineWidth(1);
