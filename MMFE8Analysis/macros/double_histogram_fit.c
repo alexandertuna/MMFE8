cb72159ac8728f8d2d9c19a3d0ea64b639832d5c
sezata <bigbengarber@gmail.com>
2016-05-30 15:19:48 -0400

    Partially debugged macro to simultaneously fit test pulse
    Still falters on a few settings, but mostly works.  I'll start
    working on the generalized fit command line program.

--- MMFE8Analysis/macros/double_histogram_fit.c
+++ MMFE8Analysis/macros/double_histogram_fit.c
@@ -3,14 +3,15 @@
 #include <TCanvas.h>
 #include <cmath>
 #include <string>
+#include <iostream>
 #include "../include/xADCBase.hh"
 
 using namespace std;
 
 /***************  RUN SETTINGS  ***************/
 const string input_file = "../../mmfe8_gui/CalibrationRoutine/mmfe8_CalibRoutine.root";
-const unsigned int tpDAC = 140;
-const unsigned int vmm = 3;
+const unsigned int tpDAC = 160;
+const unsigned int vmm = 8;
 
 
 /**************  CODE BELOW  ***************/
@@ -21,9 +22,6 @@ const string x_label = "xADC readout (counts, 12-bit 1V full-range)";
 const string y_label = "Number of events (count)";
 const string func_name = "double_gauss";
 
-// Totally jacked from https://meetingcpp.com/index.php/br/items/cpp-and-pi.html
-constexpr double const_pi() { return std::atan(1)*4; }
-
 // Function prototype
 double double_gaus_function(double* xs, double* par);
 
@@ -32,8 +30,10 @@ double double_gaus_function(double* xs, double* par);
 void double_histogram_fit(){
   // Open file
   TChain* tree = new TChain(tree_name.c_str(), tree_name.c_str());
-  tree->AddFile(filename.c_str());
+  tree->AddFile(input_file.c_str());
   xADCBase* base = new xADCBase(tree);
+
+  // cout << endl << "File opened" << endl;
 
   // Populate histogram with appropriate settings
   const float min_count = -0.5; const float max_count = 4095.5;
@@ -47,28 +47,37 @@ void double_histogram_fit(){
     }
   }
 
+  // cout << endl << "Histogram filled" << endl;
+
   // Create the double Gaussian function with the appropriate defaults
   // and limits. Parameters: {N0, mu0, sig0, N1, mu1, sig1}
   const float mu_tot = hist->GetMean();
   const float sig_tot = hist->GetStdDev();
   TF1* fun = new TF1(func_name.c_str(), double_gaus_function, min_count,
                               max_count, 6);
+  
+  // cout << endl << "Created function" << endl;
 
   // Defaults:
   fun->SetParameter(0, N / 2); // N0 = N1 = Ntot/2
   fun->SetParameter(3, N / 2);
   fun->SetParameter(1, mu_tot - sig_tot); // mu0
+  cout << endl << mu_tot - sig_tot << endl;
   fun->SetParameter(4, mu_tot + sig_tot);
-  // fun->SetParameter(2, sig_tot / 2); // sig0
-  // fun->SetParameter(5, sig_tot / 2);
+  fun->SetParameter(2, 2); // sig0 (estimate from previous xADC fits)
+  fun->SetParameter(5, 2);
+
+  // cout << endl << "Set parameters" << endl;
 
   // Limits:
-  fun->SetParLimits(0, 0, 2 * N); // 0 < N0, N1 < 2Ntot
-  fun->SetParLimits(3, 0, 2 * N);
+  fun->SetParLimits(0, 0, N); // 0 < N0, N1 < 2Ntot
+  fun->SetParLimits(3, 0, N);
   fun->SetParLimits(1, 0, mu_tot); // 0 < mu0 < mu_tot
   fun->SetParLimits(4, mu_tot, max_count);
   fun->SetParLimits(2, 0, max_count); // 0 < sig0, sig1 < large number
   fun->SetParLimits(5, 0, max_count);
+
+  // cout << endl << "Set limits" << endl;
 
   // Plot and fit
   TCanvas* can = new TCanvas("can", "can", 800, 600);
@@ -81,13 +90,17 @@ void double_histogram_fit(){
   can->SetGridy();
   can->cd();
   hist->Draw();
-  hist->Fit(func_name.c_str(), "IL");
+  // cout << endl << "Is it breaking here?" << endl;
+  hist->Fit(func_name.c_str(), "E");
   fun->Draw("same");
+
+  cout << endl << "Chi-squared = " << fun->GetChisquare() << endl;
 }
 
 // General normal distribution with integral N
 double normal_distribution(double N, double mu, double sg, double x){
-  double G = exp(-((x - mu)**2) / (2 * (sg**2))) / (sqrt(2 * const_pi()) * sg);
+  double pi = atan(1.)*4;
+  double G = exp(-((x - mu)**2) / (2 * (sg**2))) / (sqrt(2 * pi) * sg);
   return N * G;
 }
 
